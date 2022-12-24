bd49a8ba83b794ed0b7d0c9a9cd450cbdeaecb93
Kartavya Shankar <kartavyashankar@gmail.com>
2022-12-25 00:03:46 +0530

    Changed DockerHandler from static to SpringBean

--- src/main/java/com/openide/javascriptinterpreterengine/service/daemon/impl/DaemonServiceImpl.java
+++ src/main/java/com/openide/javascriptinterpreterengine/service/daemon/impl/DaemonServiceImpl.java
@@ -2,13 +2,18 @@ package com.openide.javascriptinterpreterengine.service.daemon.impl;
 
 import com.openide.javascriptinterpreterengine.service.daemon.DaemonService;
 import com.openide.javascriptinterpreterengine.utils.DockerHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 @Service("daemon-impl")
 public class DaemonServiceImpl implements DaemonService {
+    @Autowired
+    @Qualifier("docker-handler")
+    private DockerHandler dockerHandler;
     @Override
     public String checkDaemon() throws IOException {
-        return DockerHandler.pingDaemon();
+        return dockerHandler.pingDaemon();
     }
 }
--- src/main/java/com/openide/javascriptinterpreterengine/utils/DockerHandler.java
+++ src/main/java/com/openide/javascriptinterpreterengine/utils/DockerHandler.java
@@ -4,26 +4,38 @@ package com.openide.javascriptinterpreterengine.utils;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.CreateContainerResponse;
 import com.github.dockerjava.api.command.InspectContainerResponse;
+import com.github.dockerjava.api.model.Image;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.core.DockerClientImpl;
 import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
 import com.github.dockerjava.transport.DockerHttpClient;
 import org.apache.commons.io.IOUtils;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
+@Component("docker-handler")
 public class DockerHandler {
-    private static DockerClientConfig getDockerClientConfig() {
+
+    private DockerClient dockerClient;
+
+    public DockerHandler() {
+        this.dockerClient = getDockerClient();
+    }
+
+    private DockerClientConfig getDockerClientConfig() {
         return DefaultDockerClientConfig.createDefaultConfigBuilder()
                 .withDockerCertPath("/certs/client")
                 .withDockerHost("tcp://docker:2376")
                 .withDockerTlsVerify(true).build();
     }
 
-    private static DockerHttpClient getDockerHttpClient() {
+    private DockerHttpClient getDockerHttpClient() {
         DockerClientConfig config = getDockerClientConfig();
         return new ApacheDockerHttpClient.Builder()
                 .dockerHost(config.getDockerHost())
@@ -34,11 +46,28 @@ public class DockerHandler {
     }
 
 
-    private static DockerClient getDockerClient() {
+    private DockerClient getDockerClient() {
         return DockerClientImpl.getInstance(getDockerClientConfig(), getDockerHttpClient());
     }
 
-    public static InspectContainerResponse createContainer(String image) {
+    private boolean imageExists(String image) {
+        DockerClient dockerClient = getDockerClient();
+        List<Image> imageList = dockerClient.listImagesCmd().withShowAll(true).exec();
+        for(Image currentImage : imageList) {
+            List<String> imageTags = new ArrayList<>(List.of(currentImage.getRepoTags()));
+            for(String tag : imageTags) {
+                if(tag.equals(image)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public InspectContainerResponse createContainer(String image) {
+        if(!imageExists(image)) {
+
+        }
         DockerClient dockerClient = getDockerClient();
         CreateContainerResponse container = dockerClient.createContainerCmd(image)
                 .withName("node-js")
@@ -46,24 +75,24 @@ public class DockerHandler {
         return dockerClient.inspectContainerCmd(container.getId()).exec();
     }
 
-    public static InspectContainerResponse startContainer(InspectContainerResponse container) {
+    public InspectContainerResponse startContainer(InspectContainerResponse container) {
         DockerClient dockerClient = getDockerClient();
         dockerClient.startContainerCmd(container.getId()).exec();
         return dockerClient.inspectContainerCmd(container.getId()).exec();
     }
 
-    public static InspectContainerResponse stopContainer(InspectContainerResponse container) {
+    public InspectContainerResponse stopContainer(InspectContainerResponse container) {
         DockerClient dockerClient = getDockerClient();
         dockerClient.stopContainerCmd(container.getId()).exec();
         return dockerClient.inspectContainerCmd(container.getId()).exec();
     }
 
-    public static void removeContainer(InspectContainerResponse container) {
+    public void removeContainer(InspectContainerResponse container) {
         DockerClient dockerClient = getDockerClient();
         dockerClient.removeContainerCmd(container.getId()).exec();
     }
 
-    public static String pingDaemon() throws IOException {
+    public String pingDaemon() throws IOException {
         DockerHttpClient dockerHttpClient = getDockerHttpClient();
         DockerHttpClient.Request request = DockerHttpClient.Request.builder()
                 .method(DockerHttpClient.Request.Method.GET)
